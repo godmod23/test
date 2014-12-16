920dd558f20df03f69e707e77961a9767b1ec6b3
God Mod <godmod23@web.de>
2014-12-16 14:14:31 +0100

    fix

--- tests/phplint.sh
+++ tests/phplint.sh
@@ -1,3 +1,5 @@
 echo "start";
-result=find ./ -type f -name \*.php -exec php -l {} \; | grep "Errors parsing ";
-echo $result;
+find ../ -type f -name \*.php -exec php -l {} \; | grep "Errors parsing ";
+result=find ../ -type f -name \*.php -exec php -l {} \; | grep "Errors parsing " -ci;
+
+return $result;
