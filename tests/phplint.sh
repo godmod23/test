82b4e0a27fcc7a6364d50fc379b7693f0fb35c34
God Mod <godmod23@web.de>
2014-12-16 14:36:14 +0100

    gimme output

--- tests/phplint.sh
+++ tests/phplint.sh
@@ -1,4 +1,4 @@
 echo "start php syntax check";
-find ../ -type f -name \*.php -exec php -l {} \; | grep "Errors parsing ";
+find ../ -type f -name \*.php -exec php -l {} \;
 result=$(find ../ -type f -name \*.php -exec php -l {} \; | grep -ci "Errors parsing ")
 exit $result;
