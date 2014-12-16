44756495caa3dde527ad385c97cfa3f4f7c097a4
God Mod <godmod23@web.de>
2014-12-16 14:47:55 +0100

    pi

--- tests/phplint.sh
+++ tests/phplint.sh
@@ -1,4 +1,3 @@
 echo "start php syntax check";
-find ../ -type f -name \*.php -exec php -l {} \;
 result=$(find ../ -type f -name \*.php -exec php -l {} \; | grep -ci "Errors parsing ")
 exit $result;
